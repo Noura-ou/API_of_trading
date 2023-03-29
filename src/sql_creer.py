259ba9708058e7b8d04266ce91dc2723f0a9869c
Noura Ous <noura.ousfia@gmail.com>
2023-03-29 15:26:32 +0200

    create table

--- src/sql_creer.py
+++ src/sql_creer.py
@@ -13,7 +13,55 @@ cursor.execute("""
             )
 """)
 
+cursor.execute(""" 
+       CREATE TABLE IF NOT EXISTS action(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL,
+            entreprise TEXT NOT NULL,
+            price TEXT NOT NULL
+            )
+""")
 
+
+cursor.execute(""" 
+       CREATE TABLE IF NOT EXISTS following(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            FOREIGN KEY(user_id) REFERENCES user(id)
+            )
+""")
+
+cursor.execute(""" 
+       CREATE TABLE IF NOT EXISTS user_actions(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            action_id INTEGER,
+            FOREIGN KEY(user_id) REFERENCES user(id),
+            FOREIGN KEY(action_id) REFERENCES action(id)
+            )
+""")
+
+cursor.execute(""" 
+       CREATE TABLE IF NOT EXISTS sell_order(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            action_id INTEGER,
+            date_vente TEXT NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES user(id),
+            FOREIGN KEY(action_id) REFERENCES action(id)
+            )
+""")
+
+cursor.execute(""" 
+       CREATE TABLE IF NOT EXISTS buyed_order(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            action_id INTEGER,
+            date_achat TEXT NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES user(id),
+            FOREIGN KEY(action_id) REFERENCES action(id)
+            )
+""")
 
 
 connection.commit()
