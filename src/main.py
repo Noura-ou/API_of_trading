5ab6211b52f36a13514ea43695b64a42dea7f6ae
Noura Ous <noura.ousfia@gmail.com>
2023-03-30 11:42:56 +0200

    create key

--- src/main.py
+++ src/main.py
@@ -1,8 +1,40 @@
 from fastapi import FastAPI
+import sql_crud_test
+from fastapi import FastAPI, HTTPException, Request, Depends
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+import hashlib
+import os
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
 
 app = FastAPI()
 
+# Fonctions utiles :
+def hasher_mdp(mdp:str) -> str:
+    return hashlib.sha256(mdp.encode()).hexdigest()
 
+def decoder_token(token:str)->dict:
+    return jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+
+def verifier_token(req: Request):
+    token = req.headers["Authorization"]
+
+# Classes contenu
+class UserRegister(BaseModel):
+    nom:str
+    email:str
+    mdp:str
+
+class UserLogin(BaseModel):
+    email:str
+    mdp:str
+
+
+# Début des endpoints
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -12,6 +44,23 @@ async def root():
 async def test():
     return {"message": "Bonsoir"}
 
-@app.post("/test/inscription")
-async def registration():
-    
+
+@app.post("/api/auth/inscription")
+async def inscription(user:UserRegister):
+    if len(sql_crud_test.get_users_by_mail(user.email)) > 0:
+        raise HTTPException(status_code=403, detail="L'email fourni possède déjà un compte")
+    else:
+        id_user = sql_crud_test.creer_utilisateur(user.nom, user.email, hasher_mdp(user.mdp), None)
+        token = jwt.encode({
+            "email" : user.email,
+            "mdp" : user.mdp,
+            "id" : id_user
+        }, SECRET_KEY, algorithm=ALGORITHM)
+        sql_crud_test.update_token(id_user, token)
+        return {"token" : token}
+
+
+
+@app.get("/api/action")
+async def mes_articles(req: Request):
+   ...
